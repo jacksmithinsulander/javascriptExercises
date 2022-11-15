7afc0c1bd05ba6256695b8f0236b9ae5a15778e4
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-11-15 15:52:24 +0100

    calcfix

--- calculator/script.js
+++ calculator/script.js
@@ -3,15 +3,15 @@ let num2 = document.getElementById("num2");
 let calculateBtn = document.getElementById("calculateBtn");
 let printResult = document.getElementById("printResult");
 
-let p = document.querySelector('#calc')
+let OP = document.querySelector('#calc')
 
 calculateBtn.addEventListener("click", () => {
 	if (isNaN(num1.value) && isNaN(num2.value)) {
-		printResult.innerHTML = "Error, faulty characters";
+		results.push("Error, faulty characters");
 	} else {
 		var a = Number(num1.value);
 		var b = Number(num2.value);
-		var result= eval(a + p.value + b);
-		printResult.innerHTML = result;
+		var result= eval(a + OP.value + b);
 	}
+	printResult.insertAdjacentHTML("afterbegin", "<li>" + result  + "</li>");
 });
